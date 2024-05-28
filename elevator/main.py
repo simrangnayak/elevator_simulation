7f3e3454dbd361988594464c8bdb290e3b3e734f
Simran Nayak <simrangnayak@gmail.com>
2024-05-28 16:15:14 +0800

    adding elevator class

---
+++ elevator/main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+class Elevator(object):
+    def __init__(self, cur_floor, direction):
+        """
+        Initializing function for Elevator class
+        """
+        self.floor = cur_floor
+        self.direct = direction
+    
+    def move(self):
+        """
+        Moves an elevator by one step based on the direction
+        """
+        if self.direct == 'down':
+            self.floor -= 1
+        if self.direct == 'up':
+            self.floor +=1
